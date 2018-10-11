52add1fd6394478c780a306f678a8ed55750ac45
Nikolas9896 <nhlynka@gmail.com>
2018-10-11 13:27:34 +0300

    Made Excercise :+1:

---
+++ script1.js
@@ -0,0 +1,17 @@
+//************Get Elements BY  **********
+var idHighlight = document.getElementById("highlight");
+var classBolded = document.getElementsByClassName("bolded");
+var tag_Li = document.getElementsByTagName("li");
+// **************Query Selector and Query Selector All*************
+var h2 = document.querySelector("h2");
+var h2_All = document.querySelectorAll("h2");
+var li_idHighlight = document.querySelector("#highlight");
+var li_classBolded = document.querySelector(".bolded");
+
+//*********************************Excercise*****************************************
+//************Come up with 4 different ways to select the first <p> tag**************
+var id_p = document.getElementById("first");
+var class_p = document.getElementsByClassName("special")[0];
+var tag_p = document.getElementsByTagName("p")[0];
+var id_query_p = document.querySelector("#first");
+
