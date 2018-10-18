004f6724c51d4bc1a7677c603b85cb94d354d78c
Nikolas9896 <nhlynka@gmail.com>
2018-10-18 11:17:08 +0300

    Updated code :+1:

--- script2.js
+++ script2.js
@@ -2,11 +2,24 @@ var p_text = document.getElementsByTagName('p')[0];
 var button0 = document.getElementsByTagName('button')[0];
 var h1_js = document.querySelector('h1');
 var list_black = document.querySelectorAll('li');
+var body_background = document.querySelector('body');
+var isBlue = false;
+
 
 button0.addEventListener("click", function(){
 
 	h1_js.style.color = "Green";
 	p_text.textContent = "Goodbye!";
+	
+	if(!isBlue){
+	body_background.style.background = "lightblue";
+		
+	}
+	else{
+	body_background.style.background = "white";
+	 
+}
+	isBlue=!isBlue;
 
 })
 
@@ -15,6 +28,7 @@ for(var i = 0; i < list_black.length; i++)
 	
 	list_black[i].addEventListener("click", function(){
 		this.style.color = "orange";
+		this.textContent = "Orange";
 
 	})
 	
