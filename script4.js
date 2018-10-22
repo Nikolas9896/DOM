e0b0327608e50f6212d60ed36854ac03e5871c7f
Nikolas9896 <nhlynka@gmail.com>
2018-10-22 13:53:48 +0300

    Add this.

--- script4.js
+++ script4.js
@@ -4,11 +4,11 @@ for(i = 0; i<sizeLiAll; i++)
 {
 	LiAll[i].addEventListener("mouseover", function(){
 
-	this.style.color = "green";
+	this.classList.add("selected");
 });
 LiAll[i].addEventListener("mouseout", function(){
 
-	this.style.color = "black";
+	this.classList.remove("selected");
 });
 
 	LiAll[i].addEventListener("click", function(){
