c6c2c6977c3ba3841a0f989f72ce53445ecc86d1
Nikolas9896 <nhlynka@gmail.com>
2018-10-23 13:21:38 +0300

    Add different colors

--- scriptColorGame.js
+++ scriptColorGame.js
@@ -4,5 +4,12 @@ var colors = [
 			"rgb(255, 0, 255)",
 			"rgb(0, 255, 0)",
 			"rgb(0, 255, 255)",
-			"rgb(0, 0, 255)",
+			"rgb(0, 0, 255)"
 			];
+
+var squares = document.querySelectorAll(".square");
+var length = 6;
+for(var i = 0; i<length; i++ )
+{
+	squares[i].style.backgroundColor = colors[i];
+}
