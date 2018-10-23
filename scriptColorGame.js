26aee8e8689eca29a75d58334e74cd052510ce18
Nikolas9896 <nhlynka@gmail.com>
2018-10-23 15:25:18 +0300

    check the color choosing

--- scriptColorGame.js
+++ scriptColorGame.js
@@ -8,8 +8,30 @@ var colors = [
 			];
 
 var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById('colordisplay');
 var length = 6;
+var pickedColor = colors[3];
+colorDisplay.textContent = pickedColor;
 for(var i = 0; i<length; i++ )
 {
+	//add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
+
+
+	//add click listeners to spquares
+	squares[i].addEventListener("click", function(){
+	//grab color of clicked square
+	var clickedColor = this.style.backgroundColor;
+	if(clickedColor === pickedColor)
+		{
+			alert("Corrent");
+		}
+	else
+		{
+			alert("Wrong");
+		}
+
+	});
+
+
 }
